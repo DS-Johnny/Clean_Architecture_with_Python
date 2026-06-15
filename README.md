635749c64ec35f16381f08d48aee8dcb842269d5
Johnny <johnny.datascientist@gmail.com>
2026-06-15 16:50:42 -0300

    config: Adding pre-commit to project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clean_Architecture_with_Python
 Clean Achitecture and Clean Code Study Case
+
+>> pylint --generate-rcfile > .pylintrc
+
+>> pre-commit install
---
+++ example.py
@@ -0,0 +1,13 @@
+""" Descrição de modulo """
+
+def minha_muncao():
+    """
+    Descrição da função
+    """
+    print('OLa')
+
+def essa_e_minha_funcao():
+    """
+    Descrição da função
+    """
+    print('Ola')
