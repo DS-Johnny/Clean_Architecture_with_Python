8ffa1b484b6978c129be1bd35d0f315cbf10d147
Johnny <johnny.datascientist@gmail.com>
2026-06-17 23:35:04 -0300

    config: Adding SQLAchemy, pytest

---
+++ src/infra/db/settings/connection.py
@@ -0,0 +1,27 @@
+"""
+DBConnetionHandler
+"""
+
+from sqlalchemy import create_engine
+
+class DBConnectionHandler:
+    """DBConnectionHandler"""
+
+    def __init__(self) -> None:
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            'johnny',
+            'mypassword',
+            'localhost',
+            '3306',
+            'clean_database'
+        )
+
+        self.__engine = self.__create_database_engine()
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
