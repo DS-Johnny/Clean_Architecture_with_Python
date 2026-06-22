dd1b871be557358e042659bdbb5892fc238599d5
Johnny <johnny.datascientist@gmail.com>
2026-06-22 14:35:09 -0300

    feat: implementing UsersRepository

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -3,6 +3,7 @@ DBConnetionHandler
 """
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
     """DBConnectionHandler"""
@@ -18,6 +19,7 @@ class DBConnectionHandler:
         )
 
         self.__engine = self.__create_database_engine()
+        self.session = None
 
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
@@ -25,3 +27,11 @@ class DBConnectionHandler:
 
     def get_engine(self):
         return self.__engine
+    
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
